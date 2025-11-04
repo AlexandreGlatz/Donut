08d775cb650072b934da7b28cbd29a389dde799e
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2025-11-04 08:57:42 +0100

    [ADD] ANSI escape codes

--- .ccls-cache/@home@aglatz@Github@Donut/main.cpp
+++ .ccls-cache/@home@aglatz@Github@Donut/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#define osString "Windows";
+HANDLE hCondole = GetStdHandle(STD_OUTPUT_HANDLE);
+DWORD mode;
+GetConsoleMode(hConsole, &mode);
+SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+#endif
+#if defined(__unix__) || defined(__unix)
+#define osString "Linux"
+#endif
 
 int main(int argc, char** argv)
 {
-    std::cout<<"Hello Wworld !"<<std::endl;
+    std::cout<<"\033[2J";
+    std::cout<<"\033[H";
+    std::cout<<"\033[?25l";
+    std::cout<<"Detected OS : "<<osString<<std::endl;
+    std::cout<<"Hello World !"<<std::endl;
+    std::cout<<"\033[?25h";
     return 0;
 }
